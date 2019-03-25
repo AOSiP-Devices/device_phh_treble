92ab621a5ab5f2f94ab8d0d6be50ce5ef61f6498
Akhil Narang <akhilnarang.1999@gmail.com>
2019-03-25 17:08:32 +0530

    phh: aosip: fix build
    telephony-ext was not included as a boot jar
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -2,3 +2,5 @@ $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 $(call inherit-product, device/aosip/sepolicy/common/sepolicy.mk)
 -include vendor/aosip/build/core/config.mk
 
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
